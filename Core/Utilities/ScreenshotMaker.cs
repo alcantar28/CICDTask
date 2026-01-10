1a97a9152b950ded1c42055e1b05e757b3c552c9
Alejandra Alcántara <Alejandra.Alcantara@wolterskluwer.com>
2026-01-10 06:05:20 -0600

    Reordering code for element screenshot

--- Core/Utilities/ScreenshotMaker.cs
+++ Core/Utilities/ScreenshotMaker.cs
@@ -7,13 +7,13 @@ namespace CICD.Core.Utilities
         public static void TakeScreenshot(IWebDriver driver)
         {
             var screenshotFile = Path.Combine(Environment.CurrentDirectory, TestContext.CurrentContext.Test.MethodName + ".png");
-            var element = driver.FindElement(By.TagName("h1"));
 
             //For full page screenshot
             ITakesScreenshot ScreenshotDriver = (ITakesScreenshot)driver;
             ScreenshotDriver.GetScreenshot().SaveAsFile(screenshotFile);
 
             ////For specific element screenshot
+            //var element = driver.FindElement(By.TagName("h1"));
             //var elementScreenshot = ((ITakesScreenshot)element).GetScreenshot();
             //elementScreenshot.SaveAsFile(screenshotFile);
         }
